db81fb9e816e76fe79c2a67624805e08750eae55
Taoge <wenter.wu@gmail.com>
2019-02-28 11:44:35 +0800

    > notification polict

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ MANAGER = "manager"
 def notification_watcher():
     LOG.info("Starting repo issue watch service...")
 
-    repos = ["kubernetes/kubernetes", "prometheus/prometheus", "goharbor/harbor"]
+    repos = ["kubernetes/kubernetes", "prometheus/prometheus", "goharbor/harbor", "coredns/coredns", "envoyproxy/envoy"]
     issue_watchers = [RepoIssueWatcher(repo) for repo in repos]
     while 1:
         issues = []
@@ -53,7 +53,7 @@ def notification_consumer():
     while True:
         activities = feed[:]
         issues = [NotificationIssue.from_dict(activity.extra_context) for activity in activities]
-        title = "[GitHub Issue 提醒 ] {0} 你有 {1} 条新消息未读".format(datetime.now().replace(microsecond=0, second=0), len(activities))
+        title = "[CNCF Project 提醒 ] 有 {0} 条新 Issue ".format(len(activities))
 
         summary = title
         if len(issues) >= 1:
@@ -65,7 +65,7 @@ def notification_consumer():
 
         current_hour = datetime.now().hour
         density = hour_density((current_hour + 8) % 24)  # Asia/shanghai timeonze offset 8
-        should_sleep = int(3600 * (1.8 - density)*(1.6 - density))  # 最短sleep 30mins, 最长sleep 1.5 H
+        should_sleep = int(3600 * (2 - density)*(1.5 - density))  # 最短sleep 30mins, 最长sleep 1.5 H
         LOG.info("Current time %s, should sleep %s ", datetime.now(), should_sleep)
         time.sleep(should_sleep)
 
